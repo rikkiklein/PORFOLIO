0bebaccccfbc18da3bc66e43397de840379226eb
Rikki Rabinowitz <rikkirabz@gmail.com>
2016-11-18 00:39:22 -0500

    more

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -15,7 +15,7 @@ class AboutMe extends Component {
             <div className="timeline-bar"></div>
           </div>
           <div className="logo-text">
-            <h2>General Assembly Web Development Immersive June - September 2016</h2>
+            <h2>General Assembly Web Development Immersive <br/> June - September 2016</h2>
             <p>General Assembly's Web Development Immersive teaches their students how to set up their development environment, work efficiently as a developer, and make their projects a success.
             </p>
             <p>The students appropriately model their data, develop simple wireframes, and learn to track a collaborative development process using version control.
@@ -39,7 +39,7 @@ class AboutMe extends Component {
             <div className="timeline-bar"></div>
           </div>
           <div className="logo-text">
-            <h2>Brooklyn College Internship January 2015 - January 2016</h2>
+            <h2>Brooklyn College Internship<br/>  January 2015 - January 2016</h2>
             <h4>MYSQL/WEB DEVELOPER </h4>
             <p>
               Worked under the supervision of Distinguished Professor Theodore Raphan Ph.D to develop and maintain a scalable PHP based web application with a MySQL back-end to track the students, teachers and courses for the STAR early college and High School Program. Developed a method for multiple users to use the application simultaneously by updating the data in real time via MySQL transactions.
@@ -62,7 +62,7 @@ class AboutMe extends Component {
             </div>
           </div>
           <div className="logo-text">
-            <h2>Brooklyn College City University of New York June <br/>2012 - May 2016</h2>
+            <h2>Brooklyn College City University of New York  <br/> June 2012 - May 2016</h2>
             <p>Graduated with Summa Cum Laude with a Bachelors of Science in Computer and Information Science and a double minor in Accounting and Multimedia</p>
             <p>
               Brooklyn College's Department of Computer and Information Science is one of the largest in metropolitan New York. Their full-time faculty consists of 30 professors, and their facilities and equipment are among the best in the city. On-campus computer hubs house more than 1,000 PCs and Macs for their students use, and wireless Internet connections are available at many locations. The department is also an active participant in the PIMA-MFA program.
--- src/Components/Column2.js
+++ src/Components/Column2.js
@@ -6,36 +6,39 @@ class Column2 extends Component {
     return (
       <div className="App">
         <div className="col2-outer-container">
-          (917) 574-1120 <br/>
-          rifkarabinowitz@gmail.com <br/>
-          LinkedIn: <a href={"https://www.linkedin.com/in/rifkarabinowitz"} target="_blank"> rifkarabinowitz</a> <br/>
-          GitHub: <a href={"https://github.com/rikkirabz"} target="_blank"> rikkirabz</a> <br/>
-        <h2>Skills</h2>
-        <p>
-          Web Development:<br/>
-          HTML5, CSS, JavaScript, React.JS, JQuery, AJAX, JSON
-        </p>
-        <p>
-          Back-End: <br/>
-          Node.JS, MongoDB, Ruby, Ruby on rails
-        </p>
-        <p>
-          Programming: <br/>
-          MySQL, PHP, Java, C++
-        </p>
-        <p>
-          Development Tools: <br/>
-          Git, GitHub, MySQL Workbench, Eclipse, Android Studio, Atom
-        </p>
+          <div className="info">
+            (917) 574-1120 <br/>
+            rifkarabinowitz@gmail.com <br/>
+            LinkedIn: <a href={"https://www.linkedin.com/in/rifkarabinowitz"} target="_blank"> rifkarabinowitz</a> <br/>
+            GitHub: <a href={"https://github.com/rikkirabz"} target="_blank"> rikkirabz</a> <br/>
+          </div>
+          <div className="skills">
+          <h2>Skills</h2>
+          <p>
+            Web Development:<br/>
+            HTML5, CSS, JavaScript, React.JS, JQuery, AJAX, JSON
+          </p>
+          <p>
+            Back-End: <br/>
+            Node.JS, MongoDB, Ruby, Ruby on rails
+          </p>
+          <p>
+            Programming: <br/>
+            MySQL, PHP, Java, C++
+          </p>
+          <p>
+            Development Tools: <br/>
+            Git, GitHub, MySQL Workbench, Eclipse, Android Studio, Atom
+          </p>
 
-        <h2>EDUCATION</h2>
-        <p>CUNY Brooklyn College <br/>
-         BS in Computer Science <br/>
-        June 2012-May 2016
-</p>
-
+          <h2>EDUCATION</h2>
+          <p>CUNY Brooklyn College <br/>
+           BS in Computer Science <br/>
+          June 2012-May 2016
+          </p>
         </div>
       </div>
+    </div>
     );
   }
 }
